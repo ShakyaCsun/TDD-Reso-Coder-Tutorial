c367bbec355d4d6f59eeae00ae55853f60c84680
Srishan Shakya <csunshakya@gmail.com>
2020-09-18 15:42:06 +0545

    Added and Tested Remote Data Source

--- test/features/number_trivia/data/datasources/number_trivia_local_data_source_test.dart
+++ test/features/number_trivia/data/datasources/number_trivia_local_data_source_test.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:clean_architecture/core/error/exceptions.dart';
 import 'package:clean_architecture/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:shared_preferences/shared_preferences.dart';
